a877ce334032cb086026b73c175028b9632aa7cf
camila <camilafkapp@gmail.com>
2022-05-27 13:31:36 -0300

    improve css

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Styles from './Cards.module.scss'
 import { Link } from 'react-router-dom'
+import typesicons from '../Details/icons'
 
 const Cards = ({name, image, types, id}) => {
   return (
@@ -15,10 +16,13 @@ const Cards = ({name, image, types, id}) => {
 
          <img src={image} className={Styles.img} alt="Img not found" height="150px" width="100px"/> 
 
-         <div className={Styles.types}>
-            <ul>
-                {types?.map(t => (<li>{t.name }</li>))}
-            </ul>
+         <div div className={Styles.types}> 
+                {types?.map(t => (
+                  <div>
+                    <p>{t.name }</p>
+                    <img src={typesicons[t.name]} alt='' className={Styles.icon}/>
+                  </div>
+                ))}    
          </div>
 
         </div>
