56eb92156ac0d0b9eacb13df375d4fdd58b0af49
camila <camilafkapp@gmail.com>
2022-06-01 13:29:26 -0300

    changes for make it responsive

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const getPokemons = () => dispatch => {
-    return axios(`https://cfkpokeapp.herokuapp.com/pokemons`)
+    return axios(`http://localhost:3001/pokemons`)
     .then((json) => dispatch({type: 'GET_POKEMONS', payload: json.data}))
     .catch(error => console.log(error))
 }
 
 export const getPokeDetail = (id) => dispatch => {
-    return axios(`https://cfkpokeapp.herokuapp.com/pokemons/${id}`)
+    return axios(`http://localhost:3001/pokemons/${id}`)
     .then((json) => dispatch({type: 'GET_DETAIL', payload: json.data}))
     .catch(error => console.log(error))
 }
@@ -23,7 +23,7 @@ export const cleanPokemons = (dispatch) => {
 export function getPokeName(name) {
     return async function(dispatch) {
         try {
-            const json = await axios(`https://cfkpokeapp.herokuapp.com/pokemons?name=${name}`)
+            const json = await axios(`http://localhost:3001/pokemons?name=${name}`)
             return dispatch({type: 'GET_POKE_NAME', payload: json.data}) 
         } catch (error) {}
     }
@@ -32,7 +32,7 @@ export function getPokeName(name) {
 export function getAllTypes() {
     return async function(dispatch) {
         try {
-            const json = await axios.get(`https://cfkpokeapp.herokuapp.com/types`)
+            const json = await axios.get(`http://localhost:3001/types`)
             return dispatch({type: 'GET_TYPES', payload: json.data})
         } catch (error) {console.log(error)}
     }
@@ -40,7 +40,7 @@ export function getAllTypes() {
  export function postPokemon(input) {
      return async function(dispatch) {
          try {
-             let newPokemon = await axios.post('https://cfkpokeapp.herokuapp.com/pokemons', input)
+             let newPokemon = await axios.post(`http://localhost:3001/pokemons`, input)
              return dispatch ({type: 'POST_POKEMON', newPokemon})
          } catch (error) {console.log(error)}
      }
